a3ec7a2c8a1070a2537c7b0eb64d9b2301861c18
FoxMcloud5655 <support@foxmcloud.net>
2019-02-22 14:57:58 -0600

    Update OverloadBelt.java
    Fixed Draconic Belt of Overloading from crashing the client and the server due to NBT tags not being checked for.

--- src/main/java/net/foxmcloud/draconicadditions/items/baubles/OverloadBelt.java
+++ src/main/java/net/foxmcloud/draconicadditions/items/baubles/OverloadBelt.java
@@ -9,6 +9,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.MobEffects;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.potion.PotionEffect;
 
 public class OverloadBelt extends BasicBauble {
@@ -29,50 +30,69 @@ public class OverloadBelt extends BasicBauble {
 	
 	@Override
 	public boolean hasEffect(ItemStack stack) {
-		return stack.getTagCompound().getBoolean("Active");
+		if (stack.getTagCompound() != null) {
+			if (stack.getTagCompound().hasKey("Active")) {
+				return stack.getTagCompound().getBoolean("Active");
+			}
+		}
+		return false;
 	}
 	
 	@Override
 	public void onWornTick(ItemStack stack, EntityLivingBase entity) {
-		if (stack.getTagCompound().getBoolean("Active")) {
-			if (!stack.getTagCompound().getBoolean("Recharging")) {
-				final float damagePercent = 1F;
-				EntityPlayer player = (EntityPlayer)entity;
-				ArmorSummery summary = new ArmorSummery().getSummery(player);
-				int pointsToSubtract = Math.max((int)Math.ceil(summary.protectionPoints * (damagePercent/100)), 2);
-				float newEntropy = Math.min(summary.entropy + 1 + (pointsToSubtract / 20), 100F);
-	        	float totalAbsorbed = 0;
-	        	int remainingPoints = 0;
-	        	for (int i = 0; i < summary.allocation.length; i++) {
-	            	if (summary.allocation[i] == 0) continue;
-	            	ItemStack armorPeace = summary.armorStacks.get(i);
+		if (stack.getTagCompound() != null) {
+			if (stack.getTagCompound().getBoolean("Active")) {
+				if (!stack.getTagCompound().getBoolean("Recharging")) {
+					final float damagePercent = 1F;
+					EntityPlayer player = (EntityPlayer)entity;
+					ArmorSummery summary = new ArmorSummery().getSummery(player);
+					if (summary == null || summary.protectionPoints <= 0) {
+						ItemNBTHelper.setBoolean(stack, "Active", false);
+						ItemNBTHelper.setBoolean(stack, "Recharging", false);
+						player.playSound(SoundEvents.BLOCK_END_GATEWAY_SPAWN, 0.7F, 1.4F);
+						return;
+					}
+					int pointsToSubtract = Math.max((int)Math.ceil(summary.protectionPoints * (damagePercent/100)), 2);
+					float newEntropy = Math.min(summary.entropy + 1 + (pointsToSubtract / 20), 100F);
+					float totalAbsorbed = 0;
+					int remainingPoints = 0;
+					for (int i = 0; i < summary.allocation.length; i++) {
+						if (summary.allocation[i] == 0) continue;
+						ItemStack armorPeace = summary.armorStacks.get(i);
 
-	            	float dmgShear = summary.allocation[i] / summary.protectionPoints;
-	            	float dmg = dmgShear * pointsToSubtract;
+						float dmgShear = summary.allocation[i] / summary.protectionPoints;
+						float dmg = dmgShear * pointsToSubtract;
 
-	            	float absorbed = Math.min(dmg, summary.allocation[i]);
-	            	totalAbsorbed += absorbed;
-	            	summary.allocation[i] -= absorbed;
-	            	remainingPoints += summary.allocation[i];
-	            	ItemNBTHelper.setFloat(armorPeace, "ProtectionPoints", summary.allocation[i]);
-	            	ItemNBTHelper.setFloat(armorPeace, "ShieldEntropy", newEntropy);
-	        	}
-	        	//CustomArmorHandler.replaceBaubles(summary, player);
-	        	int strengthCalc = (int)(Math.round(totalAbsorbed) / 2);
-	        	if (strengthCalc > 0) {
-	        		player.removeActivePotionEffect(MobEffects.STRENGTH);
-	        		player.addPotionEffect(new PotionEffect(MobEffects.STRENGTH, 2, strengthCalc, true, false));
-	        		ItemNBTHelper.setBoolean(stack, "Recharging", true);
-	        	}
-	        	else {
-	        		player.removeActivePotionEffect(MobEffects.STRENGTH);
-	        		ItemNBTHelper.setBoolean(stack, "Active", false);
-	        		ItemNBTHelper.setBoolean(stack, "Recharging", false);
-	        		player.playSound(SoundEvents.BLOCK_END_GATEWAY_SPAWN, 0.7F, 1.4F);
-	        	}
-				player.playSound(SoundEvents.BLOCK_CHORUS_FLOWER_GROW, 0.9F, (float)Math.random() + 0.5F);
+						float absorbed = Math.min(dmg, summary.allocation[i]);
+						totalAbsorbed += absorbed;
+						summary.allocation[i] -= absorbed;
+						remainingPoints += summary.allocation[i];
+						ItemNBTHelper.setFloat(armorPeace, "ProtectionPoints", summary.allocation[i]);
+						ItemNBTHelper.setFloat(armorPeace, "ShieldEntropy", newEntropy);
+					}
+					//CustomArmorHandler.replaceBaubles(summary, player);
+					int strengthCalc = (int)(Math.round(totalAbsorbed) / 2);
+					if (strengthCalc > 0) {
+						player.removeActivePotionEffect(MobEffects.STRENGTH);
+						player.addPotionEffect(new PotionEffect(MobEffects.STRENGTH, 2, strengthCalc, true, false));
+						ItemNBTHelper.setBoolean(stack, "Recharging", true);
+					}
+					else {
+						player.removeActivePotionEffect(MobEffects.STRENGTH);
+						ItemNBTHelper.setBoolean(stack, "Active", false);
+						ItemNBTHelper.setBoolean(stack, "Recharging", false);
+						player.playSound(SoundEvents.BLOCK_END_GATEWAY_SPAWN, 0.7F, 1.4F);
+					}
+					player.playSound(SoundEvents.BLOCK_CHORUS_FLOWER_GROW, 0.9F, (float)Math.random() + 0.5F);
+				}
+				else ItemNBTHelper.setBoolean(stack, "Recharging", false);
 			}
-			else ItemNBTHelper.setBoolean(stack, "Recharging", false);
+		}
+		else {
+			NBTTagCompound nbt = new NBTTagCompound();
+			nbt.setBoolean("Active", false);
+			nbt.setBoolean("Recharging", false);
+			stack.setTagCompound(nbt);
 		}
 	}
 }
