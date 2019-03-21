fca931c6e825c61753f2b10934c1617ad46a097b
FoxMcloud5655 <support@foxmcloud.net>
2019-03-20 20:15:12 -0500

    Potato Armor Config and Break Changes
    Added new config values for Energized Potato Armor that allow you to disable armor breakage and dropping materials, as well as Infused Potato Armor transforming when smacking a mob.  Also, added more details to some error messages.

--- src/main/java/net/foxmcloud/draconicadditions/items/armor/InfusedPotatoArmor.java
+++ src/main/java/net/foxmcloud/draconicadditions/items/armor/InfusedPotatoArmor.java
@@ -29,84 +29,81 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class InfusedPotatoArmor extends ItemArmor {
-    private static ArmorMaterial potatoMaterial = EnumHelper.addArmorMaterial("infusedPotatoArmor", DraconicAdditions.MODID_PREFIX + "infused_potato_armor", -1, new int[]{1, 1, 2, 1}, 0, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER, 0.0F);
+	private static ArmorMaterial potatoMaterial = EnumHelper.addArmorMaterial("infusedPotatoArmor", DraconicAdditions.MODID_PREFIX + "infused_potato_armor", -1, new int[] {1, 1, 2, 1}, 0, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER, 0.0F);
 
 	public InfusedPotatoArmor(int renderIndexIn, EntityEquipmentSlot equipmentSlotIn) {
 		super(potatoMaterial, renderIndexIn, equipmentSlotIn);
 		this.setMaxDamage(-1);
 	}
-    
+
 	public InfusedPotatoArmor(ArmorMaterial materialIn, int renderIndexIn, EntityEquipmentSlot equipmentSlotIn) {
 		super(materialIn, renderIndexIn, equipmentSlotIn);
 		this.setMaxDamage(-1);
 	}
-	
-    @Override
-    public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand)
-    {
-    	if (ArmorStats.INFUSED_POTATO_RIGHT_CLICK)
-    		return new ActionResult<ItemStack>(EnumActionResult.FAIL, transformItem(player, hand, false));
-    	else return new ActionResult<ItemStack>(EnumActionResult.PASS, player.getHeldItem(hand));
-    }
-    
-    @Override
-    public EnumActionResult onItemUseFirst(EntityPlayer player, World world, BlockPos pos, EnumFacing side, float hitX, float hitY, float hitZ, EnumHand hand)
-    {
-    	if (ArmorStats.INFUSED_POTATO_RIGHT_CLICK) {
-    		transformItem(player, hand, true);
-        	return EnumActionResult.FAIL;
-    	}
-    	else return EnumActionResult.PASS;
-    }
-    
-    @Override
-    public boolean onLeftClickEntity(ItemStack stack, EntityPlayer player, Entity entity)
-    {
-    	transformItem(player, stack, true);
-        return false;
-    }
-    
-    @Override
-    public boolean onDroppedByPlayer(ItemStack stack, EntityPlayer player)
-    {
-    	if (ArmorStats.INFUSED_POTATO_DROP) {
-    		transformItem(player, stack, true);
-        	return false;
-    	}
-    	else return true;
-    }
-    
-    private ItemStack transformItem(EntityPlayer player, EnumHand hand, boolean replace) {
-    	ItemStack itemStack = player.getHeldItem(hand);
-    	return transformItem(player, itemStack, replace);
-    }
-    
-    private ItemStack transformItem(EntityPlayer player, ItemStack stack, boolean replace) {
-    	ItemArmor item = (ItemArmor)stack.getItem();
-    	EntityEquipmentSlot slot = item.getEquipmentSlot();
-    	PotatoArmor armor;
-    	if (slot.equals(EntityEquipmentSlot.HEAD)) armor = DAFeatures.potatoHelm;
-    	else if (slot.equals(EntityEquipmentSlot.CHEST)) armor = DAFeatures.potatoChest;
-    	else if (slot.equals(EntityEquipmentSlot.LEGS)) armor = DAFeatures.potatoLegs;
-    	else if (slot.equals(EntityEquipmentSlot.FEET)) armor = DAFeatures.potatoBoots;
-    	else throw new Error();
-    	ItemStack armorItem = new ItemStack(armor);
-    	NBTTagCompound nbt = new NBTTagCompound();
-    	nbt.setFloat("Energy", ArmorStats.POTATO_BASE_CAPACITY);
-    	armorItem.setTagCompound(nbt);
-    	player.inventory.deleteStack(stack);
-    	if (replace) {
-    		player.addItemStackToInventory(armorItem);
-        	player.sendStatusMessage(new TextComponentTranslation("msg.da.infusedTransformation.smack"), true);
-    	}
-    	else player.sendStatusMessage(new TextComponentTranslation("msg.da.infusedTransformation.normal"), true);
-    	return armorItem;
-    }
-    
-    @SideOnly(Side.CLIENT)
-    @Override
-    public void addInformation(ItemStack stack, @Nullable World playerIn, List<String> tooltip, ITooltipFlag advanced) {
-    	tooltip.add(I18n.format("item.draconicadditions:infused_potato.lore"));
-    	tooltip.add(I18n.format("item.draconicadditions:infused_potato.lore2"));
-    }
+
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand) {
+		if (ArmorStats.INFUSED_POTATO_RIGHT_CLICK) return new ActionResult<ItemStack>(EnumActionResult.FAIL, transformItem(player, hand, false));
+		else return new ActionResult<ItemStack>(EnumActionResult.PASS, player.getHeldItem(hand));
+	}
+
+	@Override
+	public EnumActionResult onItemUseFirst(EntityPlayer player, World world, BlockPos pos, EnumFacing side, float hitX, float hitY, float hitZ, EnumHand hand) {
+		if (ArmorStats.INFUSED_POTATO_RIGHT_CLICK) {
+			transformItem(player, hand, true);
+			return EnumActionResult.FAIL;
+		}
+		else return EnumActionResult.PASS;
+	}
+
+	@Override
+	public boolean onLeftClickEntity(ItemStack stack, EntityPlayer player, Entity entity) {
+		if (ArmorStats.INFUSED_POTATO_RIGHT_CLICK) {
+			transformItem(player, stack, true);
+		}
+		return false;
+	}
+
+	@Override
+	public boolean onDroppedByPlayer(ItemStack stack, EntityPlayer player) {
+		if (ArmorStats.INFUSED_POTATO_DROP) {
+			transformItem(player, stack, true);
+			return false;
+		}
+		else return true;
+	}
+
+	private ItemStack transformItem(EntityPlayer player, EnumHand hand, boolean replace) {
+		ItemStack itemStack = player.getHeldItem(hand);
+		return transformItem(player, itemStack, replace);
+	}
+
+	private ItemStack transformItem(EntityPlayer player, ItemStack stack, boolean replace) {
+		ItemArmor item = (ItemArmor) stack.getItem();
+		EntityEquipmentSlot slot = item.getEquipmentSlot();
+		PotatoArmor armor;
+		if (slot.equals(EntityEquipmentSlot.HEAD)) armor = DAFeatures.potatoHelm;
+		else if (slot.equals(EntityEquipmentSlot.CHEST)) armor = DAFeatures.potatoChest;
+		else if (slot.equals(EntityEquipmentSlot.LEGS)) armor = DAFeatures.potatoLegs;
+		else if (slot.equals(EntityEquipmentSlot.FEET)) armor = DAFeatures.potatoBoots;
+		else throw new Error("Something's wrong with the G-Diffuser!  This doesn't look like a valid Potato Armor...");
+		ItemStack armorItem = new ItemStack(armor);
+		NBTTagCompound nbt = new NBTTagCompound();
+		nbt.setFloat("Energy", ArmorStats.POTATO_BASE_CAPACITY);
+		armorItem.setTagCompound(nbt);
+		player.inventory.deleteStack(stack);
+		if (replace) {
+			player.addItemStackToInventory(armorItem);
+			player.sendStatusMessage(new TextComponentTranslation("msg.da.infusedTransformation.smack"), true);
+		}
+		else player.sendStatusMessage(new TextComponentTranslation("msg.da.infusedTransformation.normal"), true);
+		return armorItem;
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void addInformation(ItemStack stack, @Nullable World playerIn, List<String> tooltip, ITooltipFlag advanced) {
+		tooltip.add(I18n.format("item.draconicadditions:infused_potato.lore"));
+		tooltip.add(I18n.format("item.draconicadditions:infused_potato.lore2"));
+	}
 }
--- src/main/java/net/foxmcloud/draconicadditions/items/armor/PotatoArmor.java
+++ src/main/java/net/foxmcloud/draconicadditions/items/armor/PotatoArmor.java
@@ -28,148 +28,148 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class PotatoArmor extends WyvernArmor {
-	
-    private static ArmorMaterial potatoMaterial = EnumHelper.addArmorMaterial("potatoArmor", DraconicAdditions.MODID_PREFIX + "potato_armor", -1, new int[]{1, 1, 2, 1}, 0, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER, 0.0F);
-    
-    public PotatoArmor(int renderIndexIn, EntityEquipmentSlot equipmentSlotIn) {
-        super(potatoMaterial, renderIndexIn, equipmentSlotIn);
-    }
 
-    public PotatoArmor(ArmorMaterial materialIn, int renderIndexIn, EntityEquipmentSlot equipmentSlotIn) {
-        super(materialIn, renderIndexIn, equipmentSlotIn);
-    }
-    
-    @Override
-    public int getMaxUpgradeLevel(ItemStack stack, String upgrade) {
-        return ArmorStats.POTATO_UPGRADE_LEVEL;
-    }
-    
-    public ItemConfigFieldRegistry getFields(ItemStack stack, ItemConfigFieldRegistry registry) {
-        return registry;
-    }
+	private static ArmorMaterial potatoMaterial = EnumHelper.addArmorMaterial("potatoArmor", DraconicAdditions.MODID_PREFIX + "potato_armor", -1, new int[] {1, 1, 2, 1}, 0, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER, 0.0F);
 
-    @SideOnly(Side.CLIENT)
-    public ModelBiped model;
-    
-    @SideOnly(Side.CLIENT)
-    @Override
-    public ModelBiped getArmorModel(EntityLivingBase entityLiving, ItemStack itemStack, EntityEquipmentSlot armorSlot, ModelBiped _default) {
-        if (DEConfig.disable3DModels) {
-            return super.getArmorModel(entityLiving, itemStack, armorSlot, _default);
-        }
+	public PotatoArmor(int renderIndexIn, EntityEquipmentSlot equipmentSlotIn) {
+		super(potatoMaterial, renderIndexIn, equipmentSlotIn);
+	}
 
-        if (model == null) {
-            if (armorType == EntityEquipmentSlot.HEAD) model = new ModelPotatoArmor(0.5F, true, false, false, false);
-            else if (armorType == EntityEquipmentSlot.CHEST)
-                model = new ModelPotatoArmor(1.5F, false, true, false, false);
-            else if (armorType == EntityEquipmentSlot.LEGS)
-                model = new ModelPotatoArmor(1.5F, false, false, true, false);
-            else model = new ModelPotatoArmor(1F, false, false, false, true);
-            this.model.bipedHead.showModel = (armorType == EntityEquipmentSlot.HEAD);
-            this.model.bipedHeadwear.showModel = (armorType == EntityEquipmentSlot.HEAD);
-            this.model.bipedBody.showModel = ((armorType == EntityEquipmentSlot.CHEST) || (armorType == EntityEquipmentSlot.LEGS));
-            this.model.bipedLeftArm.showModel = (armorType == EntityEquipmentSlot.CHEST);
-            this.model.bipedRightArm.showModel = (armorType == EntityEquipmentSlot.CHEST);
-            this.model.bipedLeftLeg.showModel = (armorType == EntityEquipmentSlot.LEGS || armorType == EntityEquipmentSlot.FEET);
-            this.model.bipedRightLeg.showModel = (armorType == EntityEquipmentSlot.LEGS || armorType == EntityEquipmentSlot.FEET);
-        }
-        
-        if (entityLiving == null) {
-            return model;
-        }
+	public PotatoArmor(ArmorMaterial materialIn, int renderIndexIn, EntityEquipmentSlot equipmentSlotIn) {
+		super(materialIn, renderIndexIn, equipmentSlotIn);
+	}
 
-        this.model.isSneak = entityLiving.isSneaking();
-        this.model.isRiding = entityLiving.isRiding();
-        this.model.isChild = entityLiving.isChild();
+	@Override
+	public int getMaxUpgradeLevel(ItemStack stack, String upgrade) {
+		return ArmorStats.POTATO_UPGRADE_LEVEL;
+	}
 
-        this.model.bipedHeadwear.showModel = (armorType == EntityEquipmentSlot.HEAD);
-        this.model.bipedBody.showModel = ((armorType == EntityEquipmentSlot.CHEST) || (armorType == EntityEquipmentSlot.LEGS));
-        this.model.bipedLeftArm.showModel = (armorType == EntityEquipmentSlot.CHEST);
-        this.model.bipedRightArm.showModel = (armorType == EntityEquipmentSlot.CHEST);
-        this.model.bipedLeftLeg.showModel = (armorType == EntityEquipmentSlot.LEGS || armorType == EntityEquipmentSlot.FEET);
-        this.model.bipedRightLeg.showModel = (armorType == EntityEquipmentSlot.LEGS || armorType == EntityEquipmentSlot.FEET);
+	public ItemConfigFieldRegistry getFields(ItemStack stack, ItemConfigFieldRegistry registry) {
+		return registry;
+	}
 
-        return model;
-    }
-    
-    @Override
-    public float getProtectionPoints(ItemStack stack) {
-        float points = ArmorStats.POTATO_BASE_SHIELD_CAPACITY * getProtectionShare();
-        return points;
-    }
-    
-    @Override
-    public void onArmorTick(World world, EntityPlayer player, ItemStack stack) {
-        if (!stack.isEmpty()) {
-            PotatoArmor armor = (PotatoArmor)stack.getItem();
-            if (armor.getEnergyStored(stack) == 0 && ItemNBTHelper.getFloat(stack, "ProtectionPoints", 0) == 0) {
-            	ItemStack draconiumDust = new ItemStack(DEFeatures.draconiumDust);
-            	ItemStack diamond = new ItemStack(Items.DIAMOND);
-            	if (DEConfig.hardMode) {
-            		draconiumDust.setCount(36);
-            		diamond.setCount(18);
-            	}
-            	else {
-            		draconiumDust.setCount(4);
-            		diamond.setCount(2);
-            	}
-            	player.addItemStackToInventory(draconiumDust);
-            	player.addItemStackToInventory(diamond);
-            	player.inventory.deleteStack(stack);
-            	player.sendStatusMessage(new TextComponentTranslation("msg.da.energizedBreak"), true);
-            }
-        }
-    }
-    
-    @SideOnly(Side.CLIENT)
-    @Override
-    public void addInformation(ItemStack stack, @Nullable World playerIn, List<String> tooltip, ITooltipFlag advanced) {
-    	tooltip.add(I18n.format("item.draconicadditions:potato.lore"));
-    	tooltip.add(I18n.format("item.draconicadditions:potato.lore2"));
-    }
-    
-    @Override
-    public float getRecoveryRate(ItemStack stack) {
-        return (float)ArmorStats.POTATO_SHIELD_RECOVERY;
-    }
-    
-    @Override
-    public boolean hasHillStep(ItemStack stack, EntityPlayer player) {
-        return false;
-    }
+	@SideOnly(Side.CLIENT)
+	public ModelBiped model;
 
-    @Override
-    public float getFireResistance(ItemStack stack) {
-        return 0;
-    }
+	@SideOnly(Side.CLIENT)
+	@Override
+	public ModelBiped getArmorModel(EntityLivingBase entityLiving, ItemStack itemStack, EntityEquipmentSlot armorSlot, ModelBiped _default) {
+		if (DEConfig.disable3DModels) {
+			return super.getArmorModel(entityLiving, itemStack, armorSlot, _default);
+		}
 
-    @Override
-    public boolean[] hasFlight(ItemStack stack) {
-        return new boolean[]{false, false, false};
-    }
-    
-    @Override
-    public float getFlightSpeedModifier(ItemStack stack, EntityPlayer player) {
-        return 0;
-    }
+		if (model == null) {
+			if (armorType == EntityEquipmentSlot.HEAD) model = new ModelPotatoArmor(0.5F, true, false, false, false);
+			else if (armorType == EntityEquipmentSlot.CHEST) model = new ModelPotatoArmor(1.5F, false, true, false, false);
+			else if (armorType == EntityEquipmentSlot.LEGS) model = new ModelPotatoArmor(1.5F, false, false, true, false);
+			else model = new ModelPotatoArmor(1F, false, false, false, true);
+			this.model.bipedHead.showModel = (armorType == EntityEquipmentSlot.HEAD);
+			this.model.bipedHeadwear.showModel = (armorType == EntityEquipmentSlot.HEAD);
+			this.model.bipedBody.showModel = ((armorType == EntityEquipmentSlot.CHEST) || (armorType == EntityEquipmentSlot.LEGS));
+			this.model.bipedLeftArm.showModel = (armorType == EntityEquipmentSlot.CHEST);
+			this.model.bipedRightArm.showModel = (armorType == EntityEquipmentSlot.CHEST);
+			this.model.bipedLeftLeg.showModel = (armorType == EntityEquipmentSlot.LEGS || armorType == EntityEquipmentSlot.FEET);
+			this.model.bipedRightLeg.showModel = (armorType == EntityEquipmentSlot.LEGS || armorType == EntityEquipmentSlot.FEET);
+		}
 
-    @Override
-    public float getFlightVModifier(ItemStack stack, EntityPlayer player) {
-        return 0;
-    }
-    
-    @Override
-    public int getEnergyPerProtectionPoint() {
-        return ArmorStats.POTATO_SHIELD_RECHARGE_COST;
-    }
-    
-    @Override
-    protected int getCapacity(ItemStack stack) {
-        return ArmorStats.POTATO_BASE_CAPACITY;
-    }
+		if (entityLiving == null) {
+			return model;
+		}
 
-    @Override
-    protected int getMaxReceive(ItemStack stack) {
-        return ArmorStats.POTATO_MAX_RECIEVE;
-    }
+		this.model.isSneak = entityLiving.isSneaking();
+		this.model.isRiding = entityLiving.isRiding();
+		this.model.isChild = entityLiving.isChild();
+
+		this.model.bipedHeadwear.showModel = (armorType == EntityEquipmentSlot.HEAD);
+		this.model.bipedBody.showModel = ((armorType == EntityEquipmentSlot.CHEST) || (armorType == EntityEquipmentSlot.LEGS));
+		this.model.bipedLeftArm.showModel = (armorType == EntityEquipmentSlot.CHEST);
+		this.model.bipedRightArm.showModel = (armorType == EntityEquipmentSlot.CHEST);
+		this.model.bipedLeftLeg.showModel = (armorType == EntityEquipmentSlot.LEGS || armorType == EntityEquipmentSlot.FEET);
+		this.model.bipedRightLeg.showModel = (armorType == EntityEquipmentSlot.LEGS || armorType == EntityEquipmentSlot.FEET);
+
+		return model;
+	}
+
+	@Override
+	public float getProtectionPoints(ItemStack stack) {
+		float points = ArmorStats.POTATO_BASE_SHIELD_CAPACITY * getProtectionShare();
+		return points;
+	}
+
+	@Override
+	public void onArmorTick(World world, EntityPlayer player, ItemStack stack) {
+		if (!stack.isEmpty() && ArmorStats.POTATO_BREAK_ON_EXHAUST) {
+			PotatoArmor armor = (PotatoArmor) stack.getItem();
+			if (armor.getEnergyStored(stack) < 1000 && ItemNBTHelper.getFloat(stack, "ProtectionPoints", 0) == 0) {
+				ItemStack draconiumDust = new ItemStack(DEFeatures.draconiumDust);
+				ItemStack diamond = new ItemStack(Items.DIAMOND);
+				if (DEConfig.hardMode) {
+					draconiumDust.setCount(36 + (int) Math.round(Math.random() * 28));
+					diamond.setCount(9 + (int) Math.round(Math.random() * 9));
+				}
+				else {
+					draconiumDust.setCount(4 + (int) Math.round(Math.random() * 3));
+					diamond.setCount(1 + (int) Math.round(Math.random()));
+				}
+				if (ArmorStats.POTATO_DROP_ON_EXHAUST) {
+					player.addItemStackToInventory(draconiumDust);
+					player.addItemStackToInventory(diamond);
+				}
+				player.inventory.deleteStack(stack);
+				player.sendStatusMessage(new TextComponentTranslation("msg.da.energizedBreak"), true);
+			}
+		}
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void addInformation(ItemStack stack, @Nullable World playerIn, List<String> tooltip, ITooltipFlag advanced) {
+		tooltip.add(I18n.format("item.draconicadditions:potato.lore"));
+		tooltip.add(I18n.format("item.draconicadditions:potato.lore2"));
+	}
+
+	@Override
+	public float getRecoveryRate(ItemStack stack) {
+		return (float) ArmorStats.POTATO_SHIELD_RECOVERY;
+	}
+
+	@Override
+	public boolean hasHillStep(ItemStack stack, EntityPlayer player) {
+		return false;
+	}
+
+	@Override
+	public float getFireResistance(ItemStack stack) {
+		return 0;
+	}
+
+	@Override
+	public boolean[] hasFlight(ItemStack stack) {
+		return new boolean[] {false, false, false};
+	}
+
+	@Override
+	public float getFlightSpeedModifier(ItemStack stack, EntityPlayer player) {
+		return 0;
+	}
+
+	@Override
+	public float getFlightVModifier(ItemStack stack, EntityPlayer player) {
+		return 0;
+	}
+
+	@Override
+	public int getEnergyPerProtectionPoint() {
+		return ArmorStats.POTATO_SHIELD_RECHARGE_COST;
+	}
+
+	@Override
+	protected int getCapacity(ItemStack stack) {
+		return ArmorStats.POTATO_BASE_CAPACITY;
+	}
+
+	@Override
+	protected int getMaxReceive(ItemStack stack) {
+		return ArmorStats.POTATO_MAX_RECIEVE;
+	}
 }
