c19f12eddb907598a5233ab5a6c21858d7d83e32
FoxMcloud5655 <support@foxmcloud.net>
2021-04-19 13:03:37 -0500

    PWC's Line to Attached Block Fixed

--- src/main/java/net/foxmcloud/draconicadditions/client/render/entity/RenderPlug.java
+++ src/main/java/net/foxmcloud/draconicadditions/client/render/entity/RenderPlug.java
@@ -1,5 +1,7 @@
 package net.foxmcloud.draconicadditions.client.render.entity;
 
+import org.lwjgl.opengl.GL11;
+
 import net.foxmcloud.draconicadditions.entity.EntityPlug;
 import net.foxmcloud.draconicadditions.items.tools.PortableWiredCharger;
 import net.foxmcloud.draconicadditions.utils.DATextures;
@@ -19,10 +21,6 @@ import net.minecraft.util.math.Vec3d;
 
 public class RenderPlug extends Render<EntityPlug> {
 
-	double xOffset = -0.45;
-	double yOffset = -0.045;
-	double zOffset = 0.4;
-
 	public RenderPlug(RenderManager renderManager) {
 		super(renderManager);
 	}
@@ -31,71 +29,35 @@ public class RenderPlug extends Render<EntityPlug> {
 	public void doRender(EntityPlug entity, double x, double y, double z, float entityYaw, float partialTicks) {
 		EntityPlayer entityplayer = entity.getPlayer();
 
-		if (entityplayer != null && !this.renderOutlines) {
-			GlStateManager.pushMatrix();
-			GlStateManager.translate((float) x, (float) y, (float) z);
-			GlStateManager.enableRescaleNormal();
-			GlStateManager.scale(0.5F, 0.5F, 0.5F);
+		if (entityplayer != null) {
 			this.bindEntityTexture(entity);
 			Tessellator tessellator = Tessellator.getInstance();
 			BufferBuilder bufferbuilder = tessellator.getBuffer();
-			int i = 1;
-			int j = 2;
-			float f = 0.0625F;
-			float f1 = 0.125F;
-			float f2 = 0.125F;
-			float f3 = 0.1875F;
-			float f4 = 1.0F;
-			float f5 = 0.5F;
-			float f6 = 0.5F;
-			GlStateManager.rotate(180.0F - this.renderManager.playerViewY, 0.0F, 1.0F, 0.0F);
-			GlStateManager.rotate((float) (this.renderManager.options.thirdPersonView == 2 ? -1 : 1) * -this.renderManager.playerViewX, 1.0F, 0.0F, 0.0F);
-
-			if (this.renderOutlines) {
-				GlStateManager.enableColorMaterial();
-				GlStateManager.enableOutlineMode(this.getTeamColor(entity));
-			}
-
-			bufferbuilder.begin(7, DefaultVertexFormats.POSITION_TEX_NORMAL);
-			bufferbuilder.pos(-0.5D, -0.5D, 0.0D).tex(0.0625D, 0.1875D).normal(0.0F, 1.0F, 0.0F).endVertex();
-			bufferbuilder.pos(0.5D, -0.5D, 0.0D).tex(0.125D, 0.1875D).normal(0.0F, 1.0F, 0.0F).endVertex();
-			bufferbuilder.pos(0.5D, 0.5D, 0.0D).tex(0.125D, 0.125D).normal(0.0F, 1.0F, 0.0F).endVertex();
-			bufferbuilder.pos(-0.5D, 0.5D, 0.0D).tex(0.0625D, 0.125D).normal(0.0F, 1.0F, 0.0F).endVertex();
-			tessellator.draw();
-
-			if (this.renderOutlines) {
-				GlStateManager.disableOutlineMode();
-				GlStateManager.disableColorMaterial();
-			}
-
-			GlStateManager.disableRescaleNormal();
-			GlStateManager.popMatrix();
 			int k = entityplayer.getPrimaryHand() == EnumHandSide.RIGHT ? 1 : -1;
 			ItemStack itemstack = entityplayer.getHeldItemMainhand();
-
 			if (!(itemstack.getItem() instanceof PortableWiredCharger)) {
 				k = -k;
 			}
 
-			float f7 = entityplayer.getSwingProgress(partialTicks);
-			float f8 = MathHelper.sin(MathHelper.sqrt(f7) * (float) Math.PI);
-			float f9 = (entityplayer.prevRenderYawOffset + (entityplayer.renderYawOffset - entityplayer.prevRenderYawOffset) * partialTicks) * 0.017453292F;
-			double d0 = (double) MathHelper.sin(f9);
-			double d1 = (double) MathHelper.cos(f9);
+			float f1 = entityplayer.getSwingProgress(partialTicks);
+			float f2 = MathHelper.sin(MathHelper.sqrt(f1) * (float) Math.PI);
+			float f3 = (entityplayer.prevRenderYawOffset + (entityplayer.renderYawOffset - entityplayer.prevRenderYawOffset) * partialTicks) * 0.017453292F;
+			double d0 = (double) MathHelper.sin(f3);
+			double d1 = (double) MathHelper.cos(f3);
 			double d2 = (double) k * 0.35D;
 			double d3 = 0.8D;
 			double d4, d5, d6, d7;
 
 			if ((this.renderManager.options == null || this.renderManager.options.thirdPersonView <= 0) && entityplayer == Minecraft.getMinecraft().player) {
-				float f10 = this.renderManager.options.fovSetting;
-				f10 = f10 / 100.0F;
-				Vec3d vec3d = new Vec3d((double) k * xOffset * (double) f10, yOffset * (double) f10, zOffset);
+				float f4 = this.renderManager.options.fovSetting;
+				f4 = f4 / 100.0F;
+				Vec3d vec3d = new Vec3d((double)k * -0.36D * (double)f4, -0.045D * (double)f4, 0.4D);
 				vec3d = vec3d.rotatePitch(-(entityplayer.prevRotationPitch + (entityplayer.rotationPitch - entityplayer.prevRotationPitch) * partialTicks) * 0.017453292F);
 				vec3d = vec3d.rotateYaw(-(entityplayer.prevRotationYaw + (entityplayer.rotationYaw - entityplayer.prevRotationYaw) * partialTicks) * 0.017453292F);
-				vec3d = vec3d.rotateYaw(f8 * 0.5F);
-				vec3d = vec3d.rotatePitch(-f8 * 0.7F);
+				vec3d = vec3d.rotateYaw(f2 * 0.5F);
+				vec3d = vec3d.rotatePitch(-f2 * 0.7F);
 				d4 = entityplayer.prevPosX + (entityplayer.posX - entityplayer.prevPosX) * (double) partialTicks + vec3d.x;
-				d5 = entityplayer.prevPosY + (entityplayer.posY - entityplayer.prevPosY) * (double) partialTicks + vec3d.y;
+				d5 = -0.3D + entityplayer.prevPosY + (entityplayer.posY - entityplayer.prevPosY) * (double) partialTicks + vec3d.y;
 				d6 = entityplayer.prevPosZ + (entityplayer.posZ - entityplayer.prevPosZ) * (double) partialTicks + vec3d.z;
 				d7 = (double) entityplayer.getEyeHeight();
 			}
@@ -119,13 +81,12 @@ public class RenderPlug extends Render<EntityPlug> {
 
 			for (int i1 = 0; i1 <= 16; ++i1) {
 				float f11 = (float) i1 / 16.0F;
-				bufferbuilder.pos(x + d10 * (double) f11, y + d11 * (double) (f11 * f11 + f11) * 0.5D + 0.25D, z + d12 * (double) f11).color(0, 0, 0, 255).endVertex();
+				bufferbuilder.pos(x + d10 * (double) f11, y + d11 * (double) (f11 * f11 + f11) * 0.5D + 0.5D, z + d12 * (double) f11).color(0, 0, 0, 255).endVertex();
 			}
 
 			tessellator.draw();
 			GlStateManager.enableLighting();
 			GlStateManager.enableTexture2D();
-			super.doRender(entity, x, y, z, entityYaw, partialTicks);
 		}
 	}
 
