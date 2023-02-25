7fc281574e5c927f5133c1e098605166bef4d463
FoxMcloud5655 <support@foxmcloud.net>
2023-02-25 03:13:26 -0600

    Made Modular Necklaces able to use Elytra Flight

--- src/main/java/net/foxmcloud/draconicadditions/items/curios/ModularNecklace.java
+++ src/main/java/net/foxmcloud/draconicadditions/items/curios/ModularNecklace.java
@@ -4,7 +4,13 @@ import static com.brandon3055.draconicevolution.init.ModuleCfg.removeInvalidModu
 
 import javax.annotation.Nullable;
 
+import com.brandon3055.brandonscore.api.ElytraEnabledItem;
+import com.brandon3055.brandonscore.api.power.IOPStorage;
+import com.brandon3055.draconicevolution.api.capability.DECapabilities;
+import com.brandon3055.draconicevolution.api.capability.ModuleHost;
 import com.brandon3055.draconicevolution.api.modules.ModuleCategory;
+import com.brandon3055.draconicevolution.api.modules.ModuleTypes;
+import com.brandon3055.draconicevolution.api.modules.entities.FlightEntity;
 import com.brandon3055.draconicevolution.api.modules.lib.ModularOPStorage;
 import com.brandon3055.draconicevolution.api.modules.lib.ModuleHostImpl;
 import com.brandon3055.draconicevolution.init.EquipCfg;
@@ -12,9 +18,13 @@ import com.brandon3055.draconicevolution.init.TechProperties;
 
 import net.foxmcloud.draconicadditions.DAConfig;
 import net.foxmcloud.draconicadditions.items.ModularEnergyItem;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.common.util.LazyOptional;
 
-public class ModularNecklace extends ModularEnergyItem {
+public class ModularNecklace extends ModularEnergyItem implements ElytraEnabledItem {
 
 	public ModularNecklace(TechProperties props) {
 		super(props);
@@ -33,4 +43,50 @@ public class ModularNecklace extends ModularEnergyItem {
 		long capacity = (long)(EquipCfg.getBaseEnergy(techLevel) * DAConfig.necklaceCapacityMultiplier);
 		return new ModularOPStorage(host, capacity, capacity / 64);
 	}
+
+    @Override
+	public boolean canElytraFlyBC(ItemStack stack, LivingEntity entity) {
+        ModuleHost host = stack.getCapability(DECapabilities.MODULE_HOST_CAPABILITY).orElseThrow(IllegalStateException::new);
+        FlightEntity flight = host.getEntitiesByType(ModuleTypes.FLIGHT).map(e -> (FlightEntity) e).findAny().orElse(null);
+        return flight != null && flight.getElytraEnabled();
+    }
+
+    @Override
+    public boolean elytraFlightTickBC(ItemStack stack, LivingEntity entity, int flightTicks) {
+        LazyOptional<IOPStorage> power = stack.getCapability(DECapabilities.OP_STORAGE);
+        boolean creative = entity instanceof Player player && player.getAbilities().instabuild;
+        power.ifPresent(storage -> {
+            int energy = EquipCfg.elytraFlightEnergy;
+            if (storage.getOPStored() < energy && !creative) {
+                storage.modifyEnergyStored(-10);
+                Vec3 motion = entity.getDeltaMovement();
+                entity.setDeltaMovement(motion.x * 0.95, motion.y > 0 ? motion.y * 0.95 : motion.y, motion.z * 0.95);
+
+            } else{
+                if (entity.isSprinting()) {
+                    ModuleHost host = stack.getCapability(DECapabilities.MODULE_HOST_CAPABILITY).orElseThrow(IllegalStateException::new);
+                    FlightEntity module = (FlightEntity)host.getEntitiesByType(ModuleTypes.FLIGHT).findAny().orElse(null);
+                    double flightSpeed = module == null ? 0 : module.getElytraBoost();
+                    if (flightSpeed > 0) {
+                        double speed = 1.5D * flightSpeed;
+                        double accel = 0.01 * flightSpeed;
+                        Vec3 look = entity.getLookAngle();
+                        Vec3 motion = entity.getDeltaMovement();
+                        entity.setDeltaMovement(motion.add(
+                                look.x * accel + (look.x * speed - motion.x) * accel,
+                                look.y * accel + (look.y * speed - motion.y) * accel,
+                                look.z * accel + (look.z * speed - motion.z) * accel
+                        ));
+                        energy += EquipCfg.getElytraEnergy(module.getModule().getModuleTechLevel()) * flightSpeed;
+                    }
+                }
+
+                if (!entity.level.isClientSide && !creative) {
+                    storage.modifyEnergyStored(-energy);
+                }
+            }
+        });
+
+        return true;
+    }
 }
