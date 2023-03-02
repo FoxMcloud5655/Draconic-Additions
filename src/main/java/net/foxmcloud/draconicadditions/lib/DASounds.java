47fbf78c692a896731514a2653b6d6626c9b936f
FoxMcloud5655 <support@foxmcloud.net>
2023-03-02 04:53:25 -0600

    Rewrote DASounds

--- src/main/java/net/foxmcloud/draconicadditions/lib/DASounds.java
+++ src/main/java/net/foxmcloud/draconicadditions/lib/DASounds.java
@@ -2,6 +2,8 @@ package net.foxmcloud.draconicadditions.lib;
 
 import static net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus.MOD;
 
+import java.lang.reflect.Field;
+
 import com.brandon3055.draconicevolution.handlers.DESounds;
 
 import net.foxmcloud.draconicadditions.DraconicAdditions;
@@ -20,11 +22,34 @@ import net.minecraftforge.registries.ObjectHolder;
 @ObjectHolder(DraconicAdditions.MODID)
 @Mod.EventBusSubscriber(modid = DraconicAdditions.MODID, bus = MOD)
 public class DASounds extends DESounds {
-	@ObjectHolder("unplug")
-	public static SoundEvent unplug;
 
+	@ObjectHolder("hermal")
+	public static SoundEvent hermal = getSoundEvent("hermal");
+	
+	@ObjectHolder("unplug")
+	public static SoundEvent unplug = getSoundEvent("unplug");
+	
+	@ObjectHolder("unplug")
+	public static SoundEvent boom = getSoundEvent("boom");
+	
+	public static SoundEvent getSoundEvent(final String soundName) {
+		final ResourceLocation soundRL = new ResourceLocation(DraconicAdditions.MODID, soundName);
+		return new SoundEvent(soundRL).setRegistryName(soundRL);
+	}
+	
+	// A little hacky, but saves on coding.  Taken from the Minecraft mod called Alex's Mobs.
 	@SubscribeEvent
 	public static void registerSounds(RegistryEvent.Register<SoundEvent> event) {
-		event.getRegistry().register(new SoundEvent(new ResourceLocation(DraconicAdditions.MODID, "unplug")).setRegistryName("unplug"));
+		try {
+			for (Field f : DASounds.class.getDeclaredFields()) {
+				Object obj = f.get(null);
+				if (obj instanceof SoundEvent) {
+					event.getRegistry().register((SoundEvent) obj);
+				}
+			}
+		}
+		catch (IllegalAccessException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
