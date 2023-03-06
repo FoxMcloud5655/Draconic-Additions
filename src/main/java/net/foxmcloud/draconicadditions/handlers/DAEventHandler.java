5eb0e7c1a76d7d28f465a0d45a136a67f0db5fda
FoxMcloud5655 <support@foxmcloud.net>
2023-03-06 02:58:10 -0600

    Restructured DAEventHandler

--- src/main/java/net/foxmcloud/draconicadditions/handlers/DAEventHandler.java
+++ src/main/java/net/foxmcloud/draconicadditions/handlers/DAEventHandler.java
@@ -6,13 +6,11 @@ import java.util.Arrays;
 import com.brandon3055.draconicevolution.api.capability.DECapabilities;
 import com.brandon3055.draconicevolution.api.capability.ModuleHost;
 import com.brandon3055.draconicevolution.api.event.ModularItemInitEvent;
-import com.brandon3055.draconicevolution.api.modules.entities.ShieldControlEntity;
 import com.brandon3055.draconicevolution.api.modules.lib.ModuleHostImpl;
 import com.brandon3055.draconicevolution.client.gui.modular.GuiModularItem;
 import com.brandon3055.draconicevolution.client.gui.modular.itemconfig.GuiConfigurableItem;
 import com.brandon3055.draconicevolution.init.DEContent;
 import com.brandon3055.draconicevolution.items.equipment.IModularArmor;
-import com.brandon3055.draconicevolution.items.equipment.IModularItem;
 
 import net.covers1624.quack.util.SneakyUtils;
 import net.foxmcloud.draconicadditions.CommonMethods.BlockStorage;
@@ -31,7 +29,6 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.food.FoodData;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -51,7 +48,6 @@ import net.minecraftforge.event.entity.living.LivingHealEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import top.theillusivec4.curios.api.CuriosApi;
 import top.theillusivec4.curios.api.SlotTypePreset;
 import top.theillusivec4.curios.api.type.ISlotType;
@@ -67,39 +63,42 @@ public class DAEventHandler {
 		}
 		Player player = event.getPlayer();
 		boolean handsAreEmpty = player.getMainHandItem().isEmpty() && player.getOffhandItem().isEmpty();
-		if (handsAreEmpty && player.isShiftKeyDown() && event.getPos().closerToCenterThan(player.position(), 2)) {
-			ISlotType backSlot = CuriosApi.getSlotHelper().getSlotType(SlotTypePreset.BACK.getIdentifier()).get();
-			IDynamicStackHandler stackHandler = CuriosApi.getCuriosHelper().getCuriosHandler(player).orElse(null).getStacksHandler(backSlot.getIdentifier()).get().getStacks();
-			if (stackHandler.getSlots() == 0) {
+		if (!handsAreEmpty || !player.isShiftKeyDown() || !event.getPos().closerToCenterThan(player.position(), 2)) {
+			return;
+		}
+		ISlotType backSlot = CuriosApi.getSlotHelper().getSlotType(SlotTypePreset.BACK.getIdentifier()).get();
+		IDynamicStackHandler stackHandler = CuriosApi.getCuriosHelper().getCuriosHandler(player).orElse(null).getStacksHandler(backSlot.getIdentifier()).get().getStacks();
+		if (stackHandler.getSlots() == 0) {
+			return;
+		}
+		ItemStack harness = ItemStack.EMPTY;
+		for (int i = 0; i < stackHandler.getSlots(); i++) {
+			if (stackHandler.getStackInSlot(i).getItem() instanceof ModularHarness) {
+				harness = stackHandler.getStackInSlot(i);
+				break;
+			}
+		}
+		if (harness.equals(ItemStack.EMPTY)) {
+			return;
+		}
+		BlockPos abovePos = event.getPos().above();
+		BlockState aboveState = world.getBlockState(abovePos);
+		if (ModularHarness.hasAttachedBlockEntity(harness, world)) {
+			if (event.getFace() != Direction.UP || aboveState.getBlock() != Blocks.AIR) {
 				return;
 			}
-			ItemStack harness = ItemStack.EMPTY;
-			for (int i = 0; i < stackHandler.getSlots(); i++) {
-				if (stackHandler.getStackInSlot(i).getItem() instanceof ModularHarness) {
-					harness = stackHandler.getStackInSlot(i);
-					break;
-				}
+			Vec2 pRot = player.getRotationVector();
+			Vec2 rotation = new Vec2(-pRot.x, pRot.y + 180);
+			if (BlockStorage.restoreBlockFromTag(world, abovePos, rotation, harness.getTag(), true, true)) {
+				player.displayClientMessage(new TranslatableComponent("info.da.modular_harness.placeSuccess"), true);
+				event.setCanceled(true);
 			}
-			if (!harness.equals(ItemStack.EMPTY)) {
-				BlockPos abovePos = event.getPos().above();
-				BlockState aboveState = world.getBlockState(abovePos);
-				if (ModularHarness.hasAttachedBlockEntity(harness, world)) {
-					if (event.getFace() == Direction.UP && aboveState.getBlock() == Blocks.AIR) {
-						Vec2 pRot = player.getRotationVector();
-						Vec2 rotation = new Vec2(-pRot.x, pRot.y + 180);
-						if (BlockStorage.restoreBlockFromTag(world, abovePos, rotation, harness.getTag(), true, true)) {
-							player.displayClientMessage(new TranslatableComponent("info.da.modular_harness.placeSuccess"), true);
-							event.setCanceled(true);
-						}
-					}
-				}
-				else if (world.getBlockState(event.getPos()).hasBlockEntity()) {
-					if (ModularHarness.storeBlockEntity(world, event.getPos(), harness, player, true)) {
-						player.displayClientMessage(new TranslatableComponent("info.da.modular_harness.storeSuccess"), true);
-					}
-					event.setCanceled(true);
-				}
+		}
+		else if (world.getBlockState(event.getPos()).hasBlockEntity()) {
+			if (ModularHarness.storeBlockEntity(world, event.getPos(), harness, player, true)) {
+				player.displayClientMessage(new TranslatableComponent("info.da.modular_harness.storeSuccess"), true);
 			}
+			event.setCanceled(true);
 		}
 	}
 	
@@ -146,37 +145,40 @@ public class DAEventHandler {
 	@OnlyIn(Dist.CLIENT)
 	@SubscribeEvent(priority = EventPriority.HIGH)
 	public void blockChaosItemMoving(MouseClickedEvent event) {
-		if (event.getScreen() instanceof AbstractContainerScreen) {
-			AbstractContainerScreen inventory = (AbstractContainerScreen)event.getScreen();
-			Slot slot = inventory.getSlotUnderMouse();
-			if (slot != null) {
-				ItemStack stack = slot.getItem();
-				if (stack != null) {
-					if ((inventory instanceof GuiModularItem || inventory instanceof GuiConfigurableItem) && event.getButton() != 1) {
-						return;
-					}
-					Minecraft mc = inventory.getMinecraft();
-					LocalPlayer player = mc.player;
-					if (stack.getItem() instanceof ChaosContainer && !player.isCreative() && ((ChaosContainer) stack.getItem()).getChaos(stack) > 0) {
-						player.displayClientMessage(new TranslatableComponent("info.da.chaos.cantmove", stack.getHoverName()), true);
-						event.setCanceled(true);
-					}
-					else if (stack.getItem() instanceof IModularArmor armor && !player.isCreative()) {
-						LazyOptional<ModuleHost> cap = stack.getCapability(DECapabilities.MODULE_HOST_CAPABILITY);
-						if (!cap.isPresent()) {
-							return;
-						}
-						ModuleHost host = cap.orElseThrow(IllegalStateException::new);
-						ArrayList<ChaosInjectorEntity> entities = ChaosInjectorEntity.getSortedListFromStream(host.getEntitiesByType(ModuleTypes.CHAOS_INJECTOR));
-						if (entities.isEmpty()) {
-							return;
-						}
-						if (entities.get(0) != null && (entities.get(0).isChaosInBlood() || entities.get(0).getRate() > 0)) {
-							player.displayClientMessage(new TranslatableComponent("info.da.chaos.cantmove", stack.getHoverName()), true);
-							event.setCanceled(true);
-						}
-					}
-				}
+		if (!(event.getScreen() instanceof AbstractContainerScreen)) {
+			return;
+		}
+		AbstractContainerScreen inventory = (AbstractContainerScreen)event.getScreen();
+		Slot slot = inventory.getSlotUnderMouse();
+		if (slot == null) {
+			return;
+		}
+		ItemStack stack = slot.getItem();
+		if (stack == null) {
+			return;
+		}
+		if ((inventory instanceof GuiModularItem || inventory instanceof GuiConfigurableItem) && event.getButton() != 1) {
+			return;
+		}
+		Minecraft mc = inventory.getMinecraft();
+		LocalPlayer player = mc.player;
+		if (stack.getItem() instanceof ChaosContainer && !player.isCreative() && ((ChaosContainer) stack.getItem()).getChaos(stack) > 0) {
+			player.displayClientMessage(new TranslatableComponent("info.da.chaos.cantmove", stack.getHoverName()), true);
+			event.setCanceled(true);
+		}
+		else if (stack.getItem() instanceof IModularArmor armor && !player.isCreative()) {
+			LazyOptional<ModuleHost> cap = stack.getCapability(DECapabilities.MODULE_HOST_CAPABILITY);
+			if (!cap.isPresent()) {
+				return;
+			}
+			ModuleHost host = cap.orElseThrow(IllegalStateException::new);
+			ArrayList<ChaosInjectorEntity> entities = ChaosInjectorEntity.getSortedListFromStream(host.getEntitiesByType(ModuleTypes.CHAOS_INJECTOR));
+			if (entities.isEmpty()) {
+				return;
+			}
+			if (entities.get(0) != null && (entities.get(0).isChaosInBlood() || entities.get(0).getRate() > 0)) {
+				player.displayClientMessage(new TranslatableComponent("info.da.chaos.cantmove", stack.getHoverName()), true);
+				event.setCanceled(true);
 			}
 		}
 	}
@@ -201,15 +203,17 @@ public class DAEventHandler {
 	
 	@SubscribeEvent
 	public void onEntitySpawn(EntityJoinWorldEvent e) {
-		if (e.getEntity() instanceof ItemEntity) {
-			ItemEntity entity = (ItemEntity)e.getEntity();
-			ItemStack stack = entity.getItem();
-			if (stack.getItem() == DEContent.dragon_heart) {
-				CompoundTag nbt = entity.getPersistentData();
-				if (nbt != null && nbt.contains("guardian_heart") && nbt.getBoolean("guardian_heart")) {
-					entity.setItem(new ItemStack(DAContent.chaosHeart));
-				}
-			}
+		if (!(e.getEntity() instanceof ItemEntity)) {
+			return;
+		}
+		ItemEntity entity = (ItemEntity)e.getEntity();
+		ItemStack stack = entity.getItem();
+		if (stack.getItem() != DEContent.dragon_heart) {
+			return;
+		}
+		CompoundTag nbt = entity.getPersistentData();
+		if (nbt != null && nbt.contains("guardian_heart") && nbt.getBoolean("guardian_heart")) {
+			entity.setItem(new ItemStack(DAContent.chaosHeart));
 		}
 	}
 }
